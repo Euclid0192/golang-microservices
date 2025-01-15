e673ab0cf2684d8a4d29b1ef7bd4fe3d193f53a6
Nam Nguyen <nguy1053@msu.edu>
2025-01-15 18:52:03 -0500

    Establish grpc connection between gateway and order service

---
+++ common/json.go
@@ -0,0 +1,25 @@
+package common
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func ReadJSON(r *http.Request, v any) error {
+	if r.Body == nil {
+		return fmt.Errorf("missing request body")
+	}
+
+	return json.NewDecoder(r.Body).Decode(v)
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(v)
+}
+
+func WriteError(w http.ResponseWriter, status int, message string) {
+	WriteJSON(w, status, map[string]string{"error": message})
+}
--- orders/main.go
+++ orders/main.go
@@ -1,11 +1,37 @@
 package main
 
-import "context"
+import (
+	"context"
+	"log"
+	"net"
+
+	common "github.com/Euclid0192/commons"
+	"google.golang.org/grpc"
+)
+
+var (
+	grpcAddr = common.EnvString("GRPC_ADDR", "localhost:3000")
+)
 
 func main() {
 
+	grpcServer := grpc.NewServer()
+	l, err := net.Listen("tcp", grpcAddr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	defer l.Close()
+
 	store := NewStore()
 	service := NewService(store)
+	NewGRPCHandler(grpcServer)
 
 	service.CreateOrder(context.Background()) /// empty Context
+
+	log.Println("GRPC Server Started at", grpcAddr)
+
+	if err := grpcServer.Serve(l); err != nil {
+		log.Fatal(err.Error())
+	}
 }
