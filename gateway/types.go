720bc081f0ce3d38b246baf469ab0e2c44ecfb39
Nam Nguyen <nguy1053@msu.edu>
2025-02-02 22:13:53 -0500

    Fix json tag error on gateway types.go

--- gateway/types.go
+++ gateway/types.go
@@ -3,6 +3,6 @@ package main
 import pb "github.com/Euclid0192/commons/api"
 
 type CreateOrderRequest struct {
-	Order         *pb.Order `"json": order`
-	RedirectToURL string    `"json": redirectToURL`
+	Order         *pb.Order `json:"order"`
+	RedirectToURL string    `json:"redirectToURL"`
 }
